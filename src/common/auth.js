d8056620448b790a4dc02055f24c1a03dda9d978
keisuke <rememberthatk@icloud.com>
2017-12-01 08:32:20 +0900

    commonディレクトリを作成し、authとurlを作成

---
+++ src/common/auth.js
@@ -0,0 +1,23 @@
+import { AsyncStorage } from 'react-native';
+import axios from 'axios';
+import { USERS_URL, LOGIN_URL } from '../common/urls';
+
+const USER_KEY = 'user_key';
+
+export const isSignedIn = () => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(USER_KEY)
+      .then(res => {
+        (res !== null) ? resolve(true) : resolve(false);
+      })
+      .catch(err => reject(err));
+  });
+};
+
+export const onSignUp = (user_params) => axios.post(USERS_URL, user_params);
+
+export const onSignIn = (session_params) => axios.post(LOGIN_URL, session_params);
+
+export const onSignOut = () => AsyncStorage.removeItem(USER_KEY);
+
+export const storeToken = (accessToken) => AsyncStorage.setItem(USER_KEY, accessToken);
---
+++ src/common/urls.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'http://lala.work/api/v1';
+export const LOGIN_URL = BASE_URL + '/login';
+export const USERS_URL = BASE_URL + '/users';
+export const INFOS_URL = BASE_URL + '/info_articles';
