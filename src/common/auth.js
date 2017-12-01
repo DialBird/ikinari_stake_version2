03eb90167f10b52d8f020c1e4a09bc74424a4fe8
keisuke <rememberthatk@icloud.com>
2017-12-01 10:00:17 +0900

    authに他のaxiosメソッドを追加する

--- src/common/auth.js
+++ src/common/auth.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native';
 import axios from 'axios';
-import { USERS_URL, LOGIN_URL } from '../common/urls';
+import { USERS_URL, LOGIN_URL, INFOS_URL } from '../common/urls';
 
 const USER_KEY = 'user_key';
 
@@ -21,3 +21,9 @@ export const onSignIn = (session_params) => axios.post(LOGIN_URL, session_params
 export const onSignOut = () => AsyncStorage.removeItem(USER_KEY);
 
 export const storeToken = (accessToken) => AsyncStorage.setItem(USER_KEY, accessToken);
+
+export const getToken = () => AsyncStorage.getItem(USER_KEY);
+
+export const getProfile = (accessToken) => axios.get(USERS_URL + '/' + accessToken);
+
+export const getNews = () => axios.get(INFOS_URL);
