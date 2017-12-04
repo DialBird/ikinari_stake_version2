bf69eb3fe3defaa6f4be9319889f2368224c56cc
keisuke <rememberthatk@icloud.com>
2017-12-04 09:56:24 +0900

    Spinnerコンポーネントを追加する

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as ButtonGroupItem } from './ButtonGroupItem';
 export { default as Errors } from './Errors';
 export { default as MyStatus } from './MyStatus';
 export { default as Separator } from './Separator';
+export { default as Spinner } from './Spinner';
